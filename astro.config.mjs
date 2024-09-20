bd6499243690920ea934885051bbecab318a765d
YuzheZhang0 <134558726+YuzheZhang0@users.noreply.github.com>
2024-09-19 21:40:19 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://yuzhe-01.github.io/",
+  base: "yuzhe-01.github.io",
   trailingSlash: "always",
   integrations: [
     tailwind(),
