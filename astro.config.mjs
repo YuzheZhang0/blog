d8632b7af7a311024c88be14db30a5c1bf04f3ee
YuzheZhang0 <134558726+YuzheZhang0@users.noreply.github.com>
2024-09-19 21:41:24 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yuzhe-01.github.io/",
-  base: "yuzhe-01.github.io",
+  site: "https://yuzhe-01.github.io",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
